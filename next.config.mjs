d5e95336260dab7eba56807cf1ee0573ec74621d
xeu <xeu@debian>
2025-12-01 20:16:18 +0800

    build: generate static webpages

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,8 @@ export default withNextra({
   images: {
     unoptimized: true,
   },
+  output: "export",
   webpack(config) {
-    console.log(`CF_PAGES=${process.env.CF_PAGES}`)
     if (process.env.CF_PAGES === '1') {
       config.cache = false
     }
